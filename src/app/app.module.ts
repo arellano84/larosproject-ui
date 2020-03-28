23ec59b9743b181eca15ff1a59e0e87182419497
Laros <ing.arellano@gmail.com>
2020-03-28 13:34:54 +0100

    13.2. Adicionando seletor de data (componente Calendar)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {InputTextModule} from 'primeng/inputtext';
 import {InputTextareaModule} from 'primeng/inputtextarea';
 import {ButtonModule} from 'primeng/button';
 import {TableModule} from 'primeng/table';
 import {TooltipModule} from 'primeng/tooltip';
+import {CalendarModule} from 'primeng/calendar';
 
 
 import { AppRoutingModule } from './app-routing.module';
@@ -25,11 +27,14 @@ import { LanzamientoRegistroComponent } from './lanzamiento-registro/lanzamiento
   imports: [
     BrowserModule,
     AppRoutingModule,
+    BrowserAnimationsModule,
+
     InputTextModule,
     ButtonModule,
     TableModule,
     TooltipModule,
-    InputTextareaModule
+    InputTextareaModule,
+    CalendarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
