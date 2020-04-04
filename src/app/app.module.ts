32bbfdda7f01982edbb6d2fa898fc157166d66fd
Laros <ing.arellano@gmail.com>
2020-04-04 13:43:26 +0200

    17.11. Alterando o locale da aplicação para pt-BR

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, LOCALE_ID  } from '@angular/core';
+
+import { registerLocaleData } from '@angular/common';
+import localePt from '@angular/common/locales/pt';
+import localeEs from '@angular/common/locales/es';
 
 import {ToastyModule} from 'ng2-toasty';
 import {ConfirmDialogModule} from 'primeng/confirmdialog';
@@ -12,6 +16,8 @@ import { CoreModule } from './core/core.module';
 import { MovimientosModule } from './movimientos/movimientos.module';
 import { PersonasModule } from './personas/personas.module';
 
+// 17.11. Alterando o locale da aplicação para pt-BR
+registerLocaleData(localeEs);
 
 @NgModule({
   declarations: [
@@ -33,7 +39,11 @@ import { PersonasModule } from './personas/personas.module';
   exports: [
 
   ],
-  providers: [ConfirmationService],
+  providers: [
+    ConfirmationService,
+    //{ provide: LOCALE_ID, useValue: 'pt-BR' }
+    { provide: LOCALE_ID, useValue: 'es-ES' }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
