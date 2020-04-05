65f9a3500176b1e60728c6e803b6b6b3d72f6331
Laros <ing.arellano@gmail.com>
2020-04-05 16:12:58 +0200

    17.14. Desafio: mensagem de erro de usuário na exclusão de pessoa

--- src/app/core/error-handler.service.ts
+++ src/app/core/error-handler.service.ts
@@ -10,10 +10,23 @@ export class ErrorHandlerService {
   constructor(private toastyService: ToastyService) { }
 
   handle(errorResponse: any) {
-    let msg: string;
+    console.log(`ErrorHandlerService.handle()...`);
 
+    let msg: string;
     if(typeof errorResponse == 'string') {
       msg = errorResponse;
+
+    } else if(//errorResponse instanceof Response && //TODO: No funciona esto...
+              errorResponse.status >= 400
+              && errorResponse.status < 500) {
+      let errors;
+      msg = 'Ocurrio um error al procesar la solicitud.';
+      try {
+        //msg = errorResponse.error[0].mensajeUsuario;
+        errors = errorResponse.json();
+        msg = errors[0].mensagemUsuario;
+      } catch (e) { }
+      console.log(msg, errorResponse);
     } else {
       msg = `Error al procesar el servicio remoto. Intente nuevamente.`;
       console.log(msg, errorResponse);
