b16b89bcf9dc558b9e1a5337a323a4afd9626384
Laros <ing.arellano@gmail.com>
2020-04-06 12:33:40 +0200

    18.2. Configurando rotas na aplicação

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule, LOCALE_ID  } from '@angular/core';
 import { registerLocaleData } from '@angular/common';
 import localePt from '@angular/common/locales/pt';
 import localeEs from '@angular/common/locales/es';
+import { Routes, RouterModule } from '@angular/router';
 
 import {ToastyModule} from 'ng2-toasty';
 import {ConfirmDialogModule} from 'primeng/confirmdialog';
@@ -16,8 +17,21 @@ import { CoreModule } from './core/core.module';
 import { MovimientosModule } from './movimientos/movimientos.module';
 import { PersonasModule } from './personas/personas.module';
 
+import { LanzamientosBusquedaComponent } from './movimientos/lanzamientos-busqueda/lanzamientos-busqueda.component';
+import { PersonasRegistroComponent } from './personas/personas-registro/personas-registro.component';
+import { PersonasBusquedaComponent } from './personas/personas-busqueda/personas-busqueda.component';
+import { LanzamientoRegistroComponent } from './movimientos/lanzamiento-registro/lanzamiento-registro.component';
+
 // 17.11. Alterando o locale da aplicação para pt-BR
 registerLocaleData(localeEs);
+
+// 18.2. Configurando rotas na aplicação
+const routes: Routes = [
+  {path: 'movimientos', component: LanzamientosBusquedaComponent},
+  {path: 'movimientos/nuevo', component: LanzamientoRegistroComponent},
+  {path: 'personas', component: PersonasBusquedaComponent},
+  {path: 'personas/nuevo', component: PersonasRegistroComponent}
+];
 
 @NgModule({
   declarations: [
@@ -28,6 +42,8 @@ registerLocaleData(localeEs);
   imports: [
     BrowserModule,
     AppRoutingModule,
+
+    RouterModule.forRoot(routes),
 
     ToastyModule.forRoot(),
     ConfirmDialogModule,
