0e77889f62da37393983a08e0f698e412834267b
Laros <ing.arellano@gmail.com>
2020-04-11 15:59:40 +0200

    20.3. Respondendo requisições com Node.js e Express

---
+++ server-express.js
@@ -0,0 +1,14 @@
+
+// 20.3. Respondendo requisições com Node.js e Express
+// despues de lanzar -->ng build --configuration=production, lanzar -->node server-express.js
+
+const express = require('express');
+const app = express();
+
+app.use(express.static(__dirname + '/dist/larosproject-ui'));
+
+app.get('/*', function(req, res) {
+  res.sendFile(__dirname + '/dist/larosproject-ui/index.html');
+});
+
+app.listen(4200);
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://larosproject-api.herokuapp.com'
+  // apiUrl: 'https://larosproject-api.herokuapp.com'
+  apiUrl: 'http://localhost:8080'
 };
