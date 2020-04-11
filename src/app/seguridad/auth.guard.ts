9b710294e2e7565cc2cf59c1e77e4de759e7e7c9
Laros <ing.arellano@gmail.com>
2020-04-11 13:57:28 +0200

    19.15. Tratando acessos de usuários deslogados na AuthGuard

--- src/app/seguridad/auth.guard.ts
+++ src/app/seguridad/auth.guard.ts
@@ -20,7 +20,20 @@ export class AuthGuard implements CanActivate {
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
       console.log('-AuthGuard.canActivate()-', next.data.roles);
-      if(next.data.roles && !this.authService.tieneCualquierPermiso(next.data.roles)) {
+
+      // 19.15. Tratando acessos de usuários deslogados na AuthGuard
+      if(this.authService.isAccessTokenInvalido()) {
+        console.log('-AuthGuard.canActivate()- Navegación con AccessToken Invalido. Obteniendo nuevo token...');
+        this.authService.obtenerNuevoAccessToken()
+          .then(() => {
+            if(this.authService.isAccessTokenInvalido()) {
+              this.router.navigate(['/login']);
+              return false;
+            }
+            return true;
+          });
+      // 19.13. Protegendo rotas com guarda de rotas (CanActivate)
+      } else if(next.data.roles && !this.authService.tieneCualquierPermiso(next.data.roles)) {
         this.router.navigate(['no-autorizado']);
         return false;
       }
